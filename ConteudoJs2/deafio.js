ae9094b47a36dc00ec83f634e06108f9f0d9ab71
Lucas Pereira <lucaspereiraprooficial@gmail.com>
2025-11-13 22:04:24 -0300

    Iniciando DOM

---
+++ ConteudoJs2/deafio.js
@@ -0,0 +1,20 @@
+const phrase = "Nome: João Silva | IDade: 28 | Email: joao.silva@gmail.com";
+
+//Verifica se tem email
+let temEmail = phrase.includes("Email");
+
+let nomeCompleto = phrase.slice(6, 16);
+
+let idade = parseInt(phrase.slice(26, 28));
+
+//pega o email completo da string
+let email = phrase.slice(38);
+
+//Verifica se tem o domínio de email
+let dominioEmail = phrase.includes("@email.com");
+
+//separa o primeiro nome
+let primeiroNome = nomeCompleto.split(" ")[0];
+
+//frase final
+console.log(primeiroNome + " tem " + idade + " anos e seu email é: " + email);
---
+++ Page/DOMeEventos.js
@@ -0,0 +1,4 @@
+const novoTexto = document.querySelector('.caixa span');
+novoTexto.textContent = 'kaik tá querendo minha pi-edade';
+novoTexto.style.color = 'red';
+
